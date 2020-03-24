43d3015a8dbcc31a757310aa750ed8d53e6ff49d
Haklerz <haakon.lervik@gmail.com>
2020-03-24 19:55:34 +0100

    Simplified the Task

--- src/com/voidfall/tasktree/Task.java
+++ src/com/voidfall/tasktree/Task.java
@@ -4,41 +4,57 @@ import java.util.Iterator;
 import java.util.Vector;
 
 /**
- * Task
+ * Represents a task with a text and subtasks.
  */
 public class Task {
     private String text;
     private Vector<Task> subTasks;
 
+    /**
+     * Creates a task with a given text.
+     * @param text The text of the task
+     */
     public Task(String text) {
         this.text = text;
         this.subTasks = new Vector<>();
     }
 
-    public Task() {
-        this.text = null;
-        this.subTasks = new Vector<>();
-    }
-
+    /**
+     * Returns the task text.
+     * @return The task text
+     */
     public String getText() {
         return text;
     }
 
-    public String getFirstLine() {
-        String firstLine = null;
-        if (text != null)
-            firstLine = text.split("\\n", 1)[0];
-        return firstLine;
-    }
-
+    /**
+     * Returns an iterator over the task's subtasks.
+     * @return An iterator over the task's subtasks
+     */
     public Iterator<Task> getSubTaskIterator() {
         return subTasks.iterator();
     }
 
-    public void addSubTask(Task task) {
-        this.subTasks.add(task);
+    /**
+     * Add a subtask to the task.
+     * @param subTask The subtask to add
+     */
+    public void addSubTask(Task subTask) {
+        this.subTasks.add(subTask);
     }
 
+    /**
+     * Remove a subtask by index.
+     * @param index The index of the subtask to remove
+     */
+    public void removeSubTask(int index) {
+        this.subTasks.remove(index);
+    }
+
+    /**
+     * Set the text of the task.
+     * @param text The text to be set
+     */
     public void setText(String text) {
         this.text = text;
     }
